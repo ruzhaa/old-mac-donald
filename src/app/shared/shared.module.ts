a42a3aa6dced57b92801bca6c7d59e64ef4078b0
Ruzha Bobotanova <r.bobotanova@webfashion.bg>
2021-06-30 14:09:19 +0300

    install(PrimeNG)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,63 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StartWithVowelPipe } from './pipes';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+// shared components
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { SongComponent } from './components/song/song.component';
+import { VerseComponent } from './components/verse/verse.component';
+
+// pipes
+import { ConsoleLogPipe } from './pipes/console-log.pipe';
+import { StartWithVowelPipe } from './pipes';
+
+// third party
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
     declarations: [
+        // shared components
+        PageHeaderComponent,
+        SongComponent,
+        VerseComponent,
+
+        // pipes
         StartWithVowelPipe,
-        PageHeaderComponent
+        ConsoleLogPipe,
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+
+        // third party
+        AutoCompleteModule,
+        ButtonModule,
+        CardModule
     ],
     exports: [
+        FormsModule,
+        ReactiveFormsModule,
+
+        // shared components
+        PageHeaderComponent,
+        SongComponent,
+        VerseComponent,
+
+        // pipes
+        ConsoleLogPipe,
         StartWithVowelPipe,
-        PageHeaderComponent
+
+        // third party
+        AutoCompleteModule,
+        ButtonModule,
+        CardModule
+
+
     ]
 })
 export class SharedModule {
